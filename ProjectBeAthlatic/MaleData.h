fa8606023530719f3a6e208e8ea3d6f21642f201
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-21 08:00:13 +0000

    create MaleData class

---
+++ ProjectBeAthlatic/MaleData.h
@@ -0,0 +1,13 @@
+//
+//  MaleData.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MaleData : NSObject
+
+@end
---
+++ ProjectBeAthlatic/MaleData.m
@@ -0,0 +1,13 @@
+//
+//  MaleData.m
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "MaleData.h"
+
+@implementation MaleData
+
+@end
