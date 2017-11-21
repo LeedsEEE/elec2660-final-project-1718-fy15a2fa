3668778e207d204ed33dd86932b204a481e2fd80
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-21 07:54:44 +0000

    create MaleProperties class

---
+++ ProjectBeAthlatic/MaleProperties.h
@@ -0,0 +1,13 @@
+//
+//  MaleProperties.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MaleProperties : NSObject
+
+@end
