d090bbf722765f176446ce47239273ac310a8d7a
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-21 09:46:44 +0000

    create female properties class in order to add properties

---
+++ ProjectBeAthlatic/FemaleProperties.h
@@ -0,0 +1,13 @@
+//
+//  FemaleProperties.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FemaleProperties : NSObject
+
+@end
---
+++ ProjectBeAthlatic/FemaleProperties.m
@@ -0,0 +1,13 @@
+//
+//  FemaleProperties.m
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "FemaleProperties.h"
+
+@implementation FemaleProperties
+
+@end
