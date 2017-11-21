53630fd0c4243d5b55c1d29e1d3fbd7625edee82
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-21 06:59:18 +0000

    Initial Commit

---
+++ ProjectBeAthlatic/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ ProjectBeAthlatic/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
