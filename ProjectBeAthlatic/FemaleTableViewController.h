570c57e272e9b569f6b3cfdd081224c594a14889
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-21 09:38:02 +0000

    create UITable/viewController for female

---
+++ ProjectBeAthlatic/FemaleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FemaleTableViewController.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FemaleTableViewController : UITableViewController
+
+@end
