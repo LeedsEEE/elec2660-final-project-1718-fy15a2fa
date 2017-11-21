6347ec3c71cb8d4c0f9cf35ca9ca2aee7e027a82
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-21 16:02:10 +0000

    I Added Navigation Controller to the second section, and then I added a button as action in order to connect to MetricViewController, finally I created UIViewController class (MetricViewController) in order to connect to the ViewController in the storyboard

---
+++ ProjectBeAthlatic/MetricViewController.h
@@ -0,0 +1,13 @@
+//
+//  MetricViewController.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MetricViewController : UIViewController
+
+@end
