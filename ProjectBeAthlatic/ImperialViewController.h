9812ff23cd764374524baa176710e7b03fd72914
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-22 19:47:40 +0000

    I created a UIViewController class called ImperialViewcontroller, and then I connected it to a view controller in the storyboard

---
+++ ProjectBeAthlatic/ImperialViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImperialViewController.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 22/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImperialViewController : UIViewController
+
+@end
