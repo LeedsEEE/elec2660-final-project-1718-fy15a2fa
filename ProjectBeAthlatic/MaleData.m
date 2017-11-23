2d4fc081e43a55f8bb70f2eecca4ff2764ee6b53
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-23 12:53:23 +0000

    add comments and pragma marks  to MaleData class

--- ProjectBeAthlatic/MaleData.m
+++ ProjectBeAthlatic/MaleData.m
@@ -10,59 +10,117 @@
 
 @implementation MaleData
 
+
+//create custom init function in order to add workouts' details
 - (instancetype)init
 {
     self = [super init];
     if (self) {
         
+        
+        
+        #pragma mark shoulders' workouts
+        
+        //set an array for shoulders' workouts by using [NSMutableArray array]
         self.ShouldersMale = [NSMutableArray array];
+        
+        //create an object for walking in order to some properties to it such as level, time , discription and photo
+        //in addition, allocate memory for the workout
         MaleProperties *www = [[MaleProperties alloc] init];
-        www.NameWorkoutMale = @"www";
-        www.LevelMale = @"Advanced";
-        www.TimeMale = @"4 minutes";
-        www.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser";
-        
-        [self.ShouldersMale addObject:www];
+        www.NameWorkoutMale = @"www"; //set workout name
+        www.LevelMale = @"Advanced"; //set the level of workout
+        www.TimeMale = @"4 minutes"; //set the time of workout
+        www.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser"; //set the discription of workout
         
         
+        #pragma mark add shoulders' workouts to the array
+        
+        //add the workouts into the shoulders array by using addObject function
+        [self.ShouldersMale addObject:www]; //This workout goes into (INDEX 0)
+        
+        
+        
+        #pragma mark arms' workouts
+        
+        //set an array for arms' workouts by using [NSMutableArray array]
         self.ArmsMale = [NSMutableArray array];
+        
+        //create an object for walking in order to some properties to it such as level, time , discription and photo
+        //in addition, allocate memory for the workout
         MaleProperties *rrr = [[MaleProperties alloc] init];
-        rrr.NameWorkoutMale = @"rrr";
-        rrr.LevelMale = @"beginner";
-        rrr.TimeMale = @"5 minutes";
-        rrr.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser";
-        
-        [self.ArmsMale addObject:rrr];
+        rrr.NameWorkoutMale = @"rrr"; //set workout name
+        rrr.LevelMale = @"beginner"; //set the level of workout
+        rrr.TimeMale = @"5 minutes"; //set the time of workout
+        rrr.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser"; //set the discription of workout
         
         
+        #pragma mark add arms' workouts to the array
+        
+        //add the workouts into the arms array by using addObject function
+        [self.ArmsMale addObject:rrr]; //This workout goes into (INDEX 0)
+        
+        
+        
+        #pragma mark legs' workouts
+        
+        //set an array for legs' workouts by using [NSMutableArray array]
         self.LegsMale = [NSMutableArray array];
+        
+        //create an object for walking in order to some properties to it such as level, time , discription and photo
+        //in addition, allocate memory for the workout
         MaleProperties *ggg = [[MaleProperties alloc] init];
-        ggg.NameWorkoutMale = @"ggg";
-        ggg.LevelMale = @"beginner";
-        ggg.TimeMale = @"5 minutes";
-        ggg.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser";
-        
-        [self.LegsMale addObject:ggg];
+        ggg.NameWorkoutMale = @"ggg"; //set workout name
+        ggg.LevelMale = @"beginner"; //set the level of workout
+        ggg.TimeMale = @"5 minutes"; //set the time of workout
+        ggg.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser"; //set the discription of workout
         
         
+        #pragma mark add legs' workouts to the array
+        
+        //add the workouts into the legs array by using addObject function
+        [self.LegsMale addObject:ggg]; //This workout goes into (INDEX 0)
+        
+        
+        
+        #pragma mark chest's workouts
+        
+        //set an array for chest's workouts by using [NSMutableArray array]
         self.ChestMale = [NSMutableArray array];
+        
+        //create an object for walking in order to some properties to it such as level, time , discription and photo
+        //in addition, allocate memory for the workout
         MaleProperties *qqq = [[MaleProperties alloc] init];
-        qqq.NameWorkoutMale = @"qqq";
-        qqq.LevelMale = @"Advanced";
-        qqq.TimeMale = @"10 minutes";
-        qqq.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser";
-        
-        [self.ChestMale addObject:qqq];
+        qqq.NameWorkoutMale = @"qqq"; //set workout name
+        qqq.LevelMale = @"Advanced"; //set the level of workout
+        qqq.TimeMale = @"10 minutes"; //set the time of workout
+        qqq.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser"; //set the discription of workout
         
         
+        #pragma mark add chest's workouts to the array
+
+        //add the workouts into the chest array by using addObject function
+        [self.ChestMale addObject:qqq]; //This workout goes into (INDEX 0)
+        
+        
+        
+        #pragma mark ABS's workouts
+        
+        //set an array for ABS's workouts by using [NSMutableArray array]
         self.ABSMale = [NSMutableArray array];
-        MaleProperties *ccc = [[MaleProperties alloc] init];
-        ccc.NameWorkoutMale = @"ccc";
-        ccc.LevelMale = @"Advanced";
-        ccc.TimeMale = @"8 minutes";
-        ccc.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser";
         
-        [self.ABSMale addObject:ccc];
+        //create an object for walking in order to some properties to it such as level, time , discription and photo
+        //in addition, allocate memory for the workout
+        MaleProperties *ccc = [[MaleProperties alloc] init];
+        ccc.NameWorkoutMale = @"ccc"; //set workout name
+        ccc.LevelMale = @"Advanced"; //set the level of workout
+        ccc.TimeMale = @"8 minutes"; //set the time of workout
+        ccc.DiscriptionMale = @"fjkjsdfk gjdklahf hdaufh hshd dushuis hsusf shusy suishisiu hisiu sius hgur suis his shsis shiuss pfjsoio fsius oisie hro g sosioser"; //set the discription of workout
+        
+        
+        #pragma mark add ABS's workouts to the array
+        
+        //add the workouts into the ABS array by using addObject function
+        [self.ABSMale addObject:ccc]; //This workout goes into (INDEX 0)
         
     }
     return self;
