f59fdb16601d1aa5e61c732c4a9e9a3e14559c13
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-23 14:49:28 +0000

    I added some comments to MetricViewController.h in order to make the code clearer

--- ProjectBeAthlatic/MetricViewController.h
+++ ProjectBeAthlatic/MetricViewController.h
@@ -10,10 +10,14 @@
 
 @interface MetricViewController : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate>
 
+// I connected BMI Value label in order to show BMI value for the users (As outlet)
 @property (weak, nonatomic) IBOutlet UILabel *BMIValueMetric;
+// I connected BMI Category label in order to show BMI category for the users (As outlet)
 @property (weak, nonatomic) IBOutlet UILabel *BMICategoryMetric;
 
+// I connected height picker in order to let the users enter their height (As outlet)
 @property (weak, nonatomic) IBOutlet UIPickerView *MetricHeightPicker;
+// I connected weight picker in order to let the users enter their weight (As outlet)
 @property (weak, nonatomic) IBOutlet UIPickerView *MetricWeightPicker;
 
 @end
