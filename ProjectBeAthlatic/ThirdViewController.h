3f56e818ec8673289e2dee8221e399a49392a615
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-27 13:43:53 +0000

    I created a part go the third section which consists of two VewControllers, two UIViewControllers, some a button

---
+++ ProjectBeAthlatic/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 27/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
