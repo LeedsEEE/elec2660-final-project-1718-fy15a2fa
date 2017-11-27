230c4c0f81c50369853cf6d8b22c00f74a83de4a
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-11-27 22:17:33 +0000

    I have created NSObject class in order to create properties that help to show physical therapy centres on the map

---
+++ ProjectBeAthlatic/PhysicalTherapyPins.h
@@ -0,0 +1,14 @@
+//
+//  PhysicalTherapyPins.h
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 27/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MapKit/MapKit.h"
+
+@interface PhysicalTherapyPins : NSObject
+
+@end
---
+++ ProjectBeAthlatic/PhysicalTherapyPins.m
@@ -0,0 +1,13 @@
+//
+//  PhysicalTherapyPins.m
+//  ProjectBeAthlatic
+//
+//  Created by Ahmad Alotaibi on 27/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "PhysicalTherapyPins.h"
+
+@implementation PhysicalTherapyPins
+
+@end
