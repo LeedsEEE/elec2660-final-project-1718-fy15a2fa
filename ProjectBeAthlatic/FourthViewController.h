dba6b0f5bd69ce4524d3365a9227ff833f6c5f45
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-12-03 21:07:14 +0000

    I have made reset button delete all the text when it is pressed

--- ProjectBeAthlatic/FourthViewController.h
+++ ProjectBeAthlatic/FourthViewController.h
@@ -10,7 +10,30 @@
 
 @interface FourthViewController : UIViewController
 
-- (IBAction)SavedButton:(UIButton *)sender; // save the text in the text fields
-- (IBAction)ResetButton:(UIButton *)sender; // delete the text in the text fields
+// buttons to save and delete the information
+- (IBAction)SavedButton:(UIButton *)sender; // save the text in the text fields (action)
+- (IBAction)ResetButton:(UIButton *)sender; // delete the text in the text fields (action)
+
+// Text fields for user's information
+@property (weak, nonatomic) IBOutlet UITextField *MyName; // set user's name (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *MyWeight;  // set user's weight (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *MyHeight;  // set user's height (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *MyAim;  // set ueser's aim (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *MyWorkout; // set user's workout (outlet)
+
+// Text fields for user's personal trainer
+@property (weak, nonatomic) IBOutlet UITextField *PTName; // personal trainer's name (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *PTPhone; // personal trainer's phone No. (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *PTEmail; //personal trianer's email (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *PTAdvice; //personal trianer's advice (outlet)
+
+// Text fields for user's physical therapist
+@property (weak, nonatomic) IBOutlet UITextField *PPName; // physical therapsit's name (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *PPPhone; // physical therapist's phone No. (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *PPEmail; // physical therapist's email. (outlet)
+@property (weak, nonatomic) IBOutlet UITextField *PPAdvice; // physical therapist's advice (outlet)
+
+
+
 
 @end
--- ProjectBeAthlatic/FourthViewController.m
+++ ProjectBeAthlatic/FourthViewController.m
@@ -90,6 +90,85 @@
                                    
                                    NSLog(@"OK");
                                    
+                                   self.MyName.text = nil;     // (1)
+                                   self.MyWeight.text = nil;   // (2)
+                                   self.MyHeight.text = nil;   // (3)
+                                   self.MyAim.text = nil;      // (4)
+                                   self.MyWorkout.text = nil;  // (5)
+                                   self.PTName.text = nil;     // (6)
+                                   self.PTPhone.text = nil;    // (7)
+                                   self.PTEmail.text = nil;    // (8)
+                                   self.PTAdvice.text = nil;   // (9)
+                                   self.PPName.text = nil;     // (10)
+                                   self.PPPhone.text = nil;    // (11)
+                                   self.PPEmail.text = nil;    // (12)
+                                   self.PPAdvice.text = nil;   // (13)
+                                   
+                                   
+                                   NSString *emptyName = self.MyName.text;  // (1)
+                                   NSUserDefaults *DefaultsEmptyName = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyName setObject:emptyName forKey:@"SaveMyName"];
+                                   [DefaultsEmptyName synchronize];
+                                   
+                                   NSString *emptyWeight = self.MyWeight.text;  // (2)
+                                   NSUserDefaults *DefaultsEmptyWeight = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyWeight setObject:emptyWeight forKey:@"SaveMyWeight"];
+                                   [DefaultsEmptyWeight synchronize];
+                                   
+                                   NSString *emptyHeight = self.MyHeight.text;  // (3)
+                                   NSUserDefaults *DefaultsEmptyHeight = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyHeight setObject:emptyHeight forKey:@"SaveMyHeight"];
+                                   [DefaultsEmptyHeight synchronize];
+                                   
+                                   NSString *emptyAim = self.MyAim.text;  // (4)
+                                   NSUserDefaults *DefaultsEmptyAim = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyAim setObject:emptyAim forKey:@"SaveMyAim"];
+                                   [DefaultsEmptyAim synchronize];
+                                   
+                                   NSString *emptyWorkout = self.MyWorkout.text;  // (5)
+                                   NSUserDefaults *DefaultsEmptyWorkout = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyWorkout setObject:emptyWorkout forKey:@"SaveMyWorkout"];
+                                   [DefaultsEmptyWorkout synchronize];
+                                   
+                                   NSString *emptyPTName = self.PTName.text;  // (6)
+                                   NSUserDefaults *DefaultsEmptyPTName = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPTName setObject:emptyPTName forKey:@"SavePTName"];
+                                   [DefaultsEmptyPTName synchronize];
+                                   
+                                   NSString *emptyPTPhone = self.PTPhone.text;  // (7)
+                                   NSUserDefaults *DefaultsEmptyPTPhone = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPTPhone setObject:emptyPTPhone forKey:@"SavePTPhone"];
+                                   [DefaultsEmptyPTPhone synchronize];
+                                   
+                                   NSString *emptyPTEmail = self.PTEmail.text;  // (8)
+                                   NSUserDefaults *DefaultsEmptyPTEmail = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPTEmail setObject:emptyPTEmail forKey:@"SavePTEmail"];
+                                   [DefaultsEmptyPTEmail synchronize];
+                                   
+                                   NSString *emptyPTAdvice = self.PTAdvice.text;  // (9)
+                                   NSUserDefaults *DefaultsEmptyPTAdvice = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPTAdvice setObject:emptyPTAdvice forKey:@"SavePTAdvice"];
+                                   [DefaultsEmptyPTAdvice synchronize];
+                                   
+                                   NSString *emptyPPName = self.PPName.text;  // (10)
+                                   NSUserDefaults *DefaultsEmptyPPName = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPPName setObject:emptyPPName forKey:@"SavePPName"];
+                                   [DefaultsEmptyPPName synchronize];
+                                   
+                                   NSString *emptyPPPhone = self.PPPhone.text;  // (11)
+                                   NSUserDefaults *DefaultsEmptyPPPhone = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPPPhone setObject:emptyPPPhone forKey:@"SavePPPhone"];
+                                   [DefaultsEmptyPPPhone synchronize];
+                                   
+                                   NSString *emptyPPEmail = self.PPEmail.text;  // (12)
+                                   NSUserDefaults *DefaultsEmptyPPEmail = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPPEmail setObject:emptyPPEmail forKey:@"SavePPEamil"];
+                                   [DefaultsEmptyPPEmail synchronize];
+                                   
+                                   NSString *emptyPPAdvice = self.PPAdvice.text;  // (13)
+                                   NSUserDefaults *DefaultsEmptyPPAdvice = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPPAdvice setObject:emptyPPAdvice forKey:@"SavePPAdvice"];
+                                   [DefaultsEmptyPPAdvice synchronize];
                                    
                                    
                                }];
