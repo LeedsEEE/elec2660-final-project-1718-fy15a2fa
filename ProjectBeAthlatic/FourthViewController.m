4cae53e53916cf8278324b772066d81bd0d66af0
Ahmad Alotaibi <ahmadalotaibi@Ahmads-MacBook-Pro.local>
2017-12-03 21:38:38 +0000

    I have made save button save the text when it is pressed

--- ProjectBeAthlatic/FourthViewController.m
+++ ProjectBeAthlatic/FourthViewController.m
@@ -17,6 +17,59 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSUserDefaults *oneSaved = [NSUserDefaults standardUserDefaults];
+    NSString *oneLoad = [oneSaved objectForKey:@"SaveMyName"];
+    [_MyName setText: oneLoad];
+    
+    NSUserDefaults *twoSaved = [NSUserDefaults standardUserDefaults]; // i did not make it NSInetger in order to allow the user save the unit (kg and Ib) which represents Strings
+    NSString *twoLoad = [twoSaved objectForKey:@"SaveMyWeight"];
+    [_MyWeight setText: twoLoad];
+    
+    NSUserDefaults *threeSaved = [NSUserDefaults standardUserDefaults]; // i did not make it NSInetger in order to allow the user save the unit (m and ft) which represents Strings
+    NSString *threeLoad = [threeSaved objectForKey:@"SaveMyHeight"];
+    [_MyHeight setText: threeLoad];
+    
+    NSUserDefaults *fourSaved = [NSUserDefaults standardUserDefaults];
+    NSString *fourLoad = [fourSaved objectForKey:@"SaveMyAim"];
+    [_MyAim setText: fourLoad];
+    
+    NSUserDefaults *fiveSaved = [NSUserDefaults standardUserDefaults];
+    NSString *fiveLoad = [fiveSaved objectForKey:@"SaveMyWorkout"];
+    [_MyWorkout setText: fiveLoad];
+    
+    NSUserDefaults *sixSaved = [NSUserDefaults standardUserDefaults];
+    NSString *sixLoad = [sixSaved objectForKey:@"SavePTName"];
+    [_PTName setText: sixLoad];
+    
+    NSUserDefaults *sevenSaved = [NSUserDefaults standardUserDefaults];
+    NSString *sevenLoad = [sevenSaved objectForKey:@"SavePTPhone"];
+    [_PTPhone setText: sevenLoad];
+    
+    NSUserDefaults *eightSaved = [NSUserDefaults standardUserDefaults];
+    NSString *eightLoad = [eightSaved objectForKey:@"SavePTEmail"];
+    [_PTEmail setText: eightLoad];
+    
+    NSUserDefaults *nineSaved = [NSUserDefaults standardUserDefaults];
+    NSString *nineLoad = [nineSaved objectForKey:@"SavePTAdvice"];
+    [_PTAdvice setText: nineLoad];
+    
+    NSUserDefaults *tenSaved = [NSUserDefaults standardUserDefaults];
+    NSString *tenLoad = [tenSaved objectForKey:@"SavePPName"];
+    [_PPName setText: tenLoad];
+    
+    NSUserDefaults *elevenSaved = [NSUserDefaults standardUserDefaults];
+    NSString *elevenLoad = [elevenSaved objectForKey:@"SavePPPhone"];
+    [_PPPhone setText: elevenLoad];
+    
+    NSUserDefaults *twelveSaved = [NSUserDefaults standardUserDefaults];
+    NSString *twelveLoad = [twelveSaved objectForKey:@"SavePPEamil"];
+    [_PPEmail setText: twelveLoad];
+    
+    NSUserDefaults *thirteenSaved = [NSUserDefaults standardUserDefaults];
+    NSString *thirteenLoad = [thirteenSaved objectForKey:@"SavePPAdvice"];
+    [_PPAdvice setText: thirteenLoad];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -57,6 +110,70 @@
                                    NSLog(@"OK");
                                    
                                    
+                                   NSString *emptyName = self.MyName.text;  // (1)
+                                   NSUserDefaults *DefaultsEmptyName = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyName setObject:emptyName forKey:@"SaveMyName"];
+                                   [DefaultsEmptyName synchronize];
+                                   
+                                   NSString *emptyWeight = self.MyWeight.text;  // (2)
+                                   NSUserDefaults *DefaultsEmptyWeight = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyWeight setObject:emptyWeight forKey:@"SaveMyWeight"];
+                                   [DefaultsEmptyWeight synchronize];
+                                   
+                                   NSString *emptyHeight = self.MyHeight.text;  // (3)
+                                   NSUserDefaults *DefaultsEmptyHeight = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyHeight setObject:emptyHeight forKey:@"SaveMyHeight"];
+                                   [DefaultsEmptyHeight synchronize];
+                                   
+                                   NSString *emptyAim = self.MyAim.text;  // (4)
+                                   NSUserDefaults *DefaultsEmptyAim = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyAim setObject:emptyAim forKey:@"SaveMyAim"];
+                                   [DefaultsEmptyAim synchronize];
+                                   
+                                   NSString *emptyWorkout = self.MyWorkout.text;  // (5)
+                                   NSUserDefaults *DefaultsEmptyWorkout = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyWorkout setObject:emptyWorkout forKey:@"SaveMyWorkout"];
+                                   [DefaultsEmptyWorkout synchronize];
+                                   
+                                   NSString *emptyPTName = self.PTName.text;  // (6)
+                                   NSUserDefaults *DefaultsEmptyPTName = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPTName setObject:emptyPTName forKey:@"SavePTName"];
+                                   [DefaultsEmptyPTName synchronize];
+                                   
+                                   NSString *emptyPTPhone = self.PTPhone.text;  // (7)
+                                   NSUserDefaults *DefaultsEmptyPTPhone = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPTPhone setObject:emptyPTPhone forKey:@"SavePTPhone"];
+                                   [DefaultsEmptyPTPhone synchronize];
+                                   
+                                   NSString *emptyPTEmail = self.PTEmail.text;  // (8)
+                                   NSUserDefaults *DefaultsEmptyPTEmail = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPTEmail setObject:emptyPTEmail forKey:@"SavePTEmail"];
+                                   [DefaultsEmptyPTEmail synchronize];
+                                   
+                                   NSString *emptyPTAdvice = self.PTAdvice.text;  // (9)
+                                   NSUserDefaults *DefaultsEmptyPTAdvice = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPTAdvice setObject:emptyPTAdvice forKey:@"SavePTAdvice"];
+                                   [DefaultsEmptyPTAdvice synchronize];
+                                   
+                                   NSString *emptyPPName = self.PPName.text;  // (10)
+                                   NSUserDefaults *DefaultsEmptyPPName = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPPName setObject:emptyPPName forKey:@"SavePPName"];
+                                   [DefaultsEmptyPPName synchronize];
+                                   
+                                   NSString *emptyPPPhone = self.PPPhone.text;  // (11)
+                                   NSUserDefaults *DefaultsEmptyPPPhone = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPPPhone setObject:emptyPPPhone forKey:@"SavePPPhone"];
+                                   [DefaultsEmptyPPPhone synchronize];
+                                   
+                                   NSString *emptyPPEmail = self.PPEmail.text;  // (12)
+                                   NSUserDefaults *DefaultsEmptyPPEmail = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPPEmail setObject:emptyPPEmail forKey:@"SavePPEamil"];
+                                   [DefaultsEmptyPPEmail synchronize];
+                                   
+                                   NSString *emptyPPAdvice = self.PPAdvice.text;  // (13)
+                                   NSUserDefaults *DefaultsEmptyPPAdvice = [NSUserDefaults standardUserDefaults];
+                                   [DefaultsEmptyPPAdvice setObject:emptyPPAdvice forKey:@"SavePPAdvice"];
+                                   [DefaultsEmptyPPAdvice synchronize];
                                    
                                }];
     
